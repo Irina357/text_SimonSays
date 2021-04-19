ce7d90a1ab83492aa73adab0ed2ae925af1eb620
iren <iren@gmail.com>
2021-04-19 11:26:20 +0200

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,207 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h1>Hello</h1>
+    <div class="container">
+      <div class="columnLeft">
+        <div v-show="activeButton">
+          <div class="imgContainer">
+            <div class="All clBlue" v-bind:class="{opacityNone: allClass[1].opac}" @click="buttonClick(1)"
+                 @click.prevent="playSound('http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3')">
+            </div>
+            <div class="All clRed" v-bind:class="{opacityNone: allClass[0].opac}" @click="buttonClick(0)"
+                 @click.prevent="playSound(require('./glass_ping-Go445-1207030150.mp3'))">
+            </div>
+          </div>
+          <div class="imgContainer">
+            <div class="All clYellow" v-bind:class="{opacityNone: allClass[3].opac}" @click="buttonClick(3)"
+                 @click.prevent="playSound(require('./glass_ping-Go445-1207030150.mp3'))">
+            </div>
+            <div class="All clGreen" v-bind:class="{opacityNone: allClass[2].opac}" @click="buttonClick(2)"
+                 @click.prevent="playSound('http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3')">
+            </div>
+          </div>
+          <div class="shadow">
+          </div>
+        </div>
+        <div v-show="passiveButton" class="passiveButton">
+          <div class="imgContainer">
+            <div class="All clBlue"></div>
+            <div class="All clRed"></div>
+          </div>
+          <div class="imgContainer">
+            <div class="All clYellow"></div>
+            <div class="All clGreen"></div>
+          </div>
+          <div class="shadow">
+          </div>
+        </div>
+      </div>
+      <div class="columnRight">
+        <h2>Round: {{ round }}</h2>
+        <div v-show="roundShow">
+          <p>Sorry, you lost after {{ round }} rounds!</p>
+        </div>
+        <button @click="clickMe(time, timeAll)">Click me</button>
+        <button @click.prevent="playSound('http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3')">
+          allPlayer
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      allClass: [
+        {
+          opac: false
+        },
+        {
+          opac: false
+        },
+        {
+          opac: false
+        },
+        {
+          opac: false
+        }
+      ],
+      allRandom: [],
+      allPlayer: [],
+      time: 1500,
+      timeAll: 4000,
+      activeButton: false,
+      passiveButton: true,
+      round: 1,
+      roundShow: false
+    }
+  },
+  methods: {
+    buttonClick(a) {
+      this.allPlayer.push(a)
+      this.allClass[a].opac = !this.allClass[a].opac
+      setTimeout(() => this.allClass[a].opac = false, 300)
+    },
+    clickMe(time, timeAll) {
+      this.allRandom = []
+      this.allPlayer = []
+      this.activeButton = !this.activeButton
+      this.passiveButton = !this.passiveButton
+      if (this.roundShow == true) {
+        this.roundShow = !this.roundShow
+      }
+      let timerId = setInterval(() => {
+        let rand = 0 + Math.random() * (4 + 0 - 0);
+        let rand1 = Math.floor(rand)
+        this.allRandom.push(rand1)
+        this.allClass[rand1].opac = !this.allClass[rand1].opac
+        if (rand1 == 0) {
+          const audio = new Audio(require('./glass_ping-Go445-1207030150.mp3'))
+          audio.play();
+        }
+        if (rand1 == 1) {
+          const audio = new Audio('http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3');
+          audio.play();
+        }
+        if (rand1 == 2) {
+          const audio = new Audio('http://soundbible.com/mp3/Elevator Ding-SoundBible.com-685385892.mp3');
+          audio.play();
+        }
+        if (rand1 == 3) {
+          const audio = new Audio(require('./glass_ping-Go445-1207030150.mp3'))
+          audio.play();
+        }
+        setTimeout(() => this.allClass[rand1].opac = false, 300)
+      }, 1500)
+      setTimeout(() => {
+        clearInterval(timerId);
+        //alert('stop');
+      }, time);
+      setTimeout(() => {
+        if (JSON.stringify(this.allRandom) === JSON.stringify(this.allPlayer)) {
+          this.activeButton = !this.activeButton
+          this.passiveButton = !this.passiveButton
+          this.round++
+          time = time + 1500
+          timeAll = timeAll + 2500
+          this.clickMe(time, timeAll)
+        } else {
+          //alert('noy')
+          this.roundShow = !this.roundShow
+          this.activeButton = !this.activeButton
+          this.passiveButton = !this.passiveButton
+          setTimeout(() => {
+            this.round = 1, this.roundShow = false
+          }, 3000)
+        }
+      }, timeAll)
+    },
+    playSound(sound) {
+      if (sound) {
+        var audio = new Audio(sound);
+        audio.play();
+      }
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="sass">
+.container, .imgContainer
+  display: flex
+  justify-content: center
+
+.imgContainer
+  position: relative
+  z-index: 100
+
+.columnLeft, .columnRight
+  width: 300px
+
+.columnRight
+  padding-left: 20px
+
+.All
+  width: 100px
+  height: 100px
+  opacity: .4
+
+.clRed
+  background: red
+  border-top-right-radius: 360%
+
+
+
+.clBlue
+  background: blue
+  border-top-left-radius: 360%
+
+
+.clGreen
+  background: green
+  border-bottom-right-radius: 360%
+
+.clYellow
+  background: yellow
+  border-bottom-left-radius: 360%
+
+.opacityNone
+  opacity: 1
+
+.passiveButton
+  position: relative
+
+.shadow
+  position: absolute
+  top: 0
+  left: 52px
+  box-shadow: 5px 5px 5px gainsboro
+  width: 203px
+  height: 203px
+  border-radius: 50%
+  background: white
 </style>
